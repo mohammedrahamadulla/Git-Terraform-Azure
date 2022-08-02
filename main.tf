2adace48963b7b9ff7299c52735dc3342f0c02bd
mohammedrahamadulla <rahamadulla786.md@gmail.com>
2022-08-02 20:20:33 +0530

    updated with a one of line as comment#

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      #version = "=3.0.0"
     }
   }
 }
@@ -12,9 +12,4 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-
-  subscription_id = "00000000-0000-0000-0000-000000000000"
-  client_id       = "00000000-0000-0000-0000-000000000000"
-  client_secret   = var.client_secret
-  tenant_id       = "00000000-0000-0000-0000-000000000000"
 }
