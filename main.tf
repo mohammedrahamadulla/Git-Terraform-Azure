9ad9c23e87be544628c449156792984367729bb2
mohammedrahamadulla <rahamadulla786.md@gmail.com>
2022-08-02 22:00:43 +0530

    Added Azure Provider Creds & .gitignore

--- main.tf
+++ main.tf
@@ -4,12 +4,16 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      #version = "=3.0.0"
+      version = "=3.16.0"
     }
   }
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {}
+  subscription_id = "aaee3ad8-43b2-4b1a-91f3-e37769180a1b"
+  client_id   	= "317c5853-26da-4808-a984-9baafac62186"
+  client_secret   = "8mi8Q~6s3QxlNJ9WrFXpyzV9W4lc9f2Hd-VqqbhS"
+  tenant_id   	= "bf9bfdb9-3fe1-41b6-9635-969e9c51c061"
+features {}
 }
