3114c843578b95818e7d64f7189f1ffcda5f56e0
root <root@ip-172-31-90-83.ec2.internal>
2022-08-08 14:49:37 +0000

    Added RG 1 2 3 rm 4

--- resource_group.tf
+++ resource_group.tf
@@ -1,14 +1,17 @@
 ## Create a resource group
 
-resource "azurerm_resource_group" "rg4" {
+resource "azurerm_resource_group" "rg1" {
   name     = "devopsb27rg4"
   location = "eastus"
 }
 
-resourc "azurerm_resourc_group" "rg1" {
-  name       = "devopsb27rg1"
-  location   = "eastus"
+resource "azurerm_resource_group" "rg2" {
+  name     = "devopsb27rg4"
+  location = "eastus"
 }
 
-
+resource "azurerm_resource_group" "rg3" {
+  name     = "devopsb27rg4"
+  location = "eastus"
+}
 
